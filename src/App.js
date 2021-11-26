643a735c7abc4af7c5c2468f5de4eee25024f3d1
c13 <chris13ryant@gmail.com>
2021-11-26 14:09:35 -0700

    added image toggle fade

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import './App.css';
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [posterImage, setPosterImage] = useState("https://www.themoviedb.org/t/p/original/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg");
-  
+  const [posterImage1, setPosterImage1] = useState("https://www.themoviedb.org/t/p/original/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg");
+  const [posterImage2, setPosterImage2] = useState("https://www.themoviedb.org/t/p/original/nx9EtxO7OuOgwmrQdHVgCpBthkw.jpg");
+  const [poster1opacity, setPoster1opacity] = useState(1);
+  const [poster2opacity, setPoster2opacity] = useState(0);
+
+  let showPoster1 = true;
+  let posterToggleTime = 5000;
+
   useEffect(() => {
     const posterTimer = setInterval(() => {
       TogglePoster();
-    }, 5000);
+    }, posterToggleTime);
     return () => clearInterval(posterTimer);
   }, []);
 
   function TogglePoster() {
-    console.log("Toggling Poster");
-    setPosterImage("https://www.themoviedb.org/t/p/original/456FcvyTujRwzgoMoKKoheeCOlU.jpg");
     
-  };
+    //setPosterImage(GetNewPoster());
+
+    setPoster1opacity(+!showPoster1);
+    setPoster2opacity(+showPoster1);
+
+    showPoster1 = !showPoster1;
+    console.log(showPoster1);
+
+    setTimeout(() => GetNewPoster(), posterToggleTime/2);
+  }
   
   
 
@@ -23,15 +36,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="Poster-Frame">
-        <img className="PosterImage" src={posterImage}></img>
-
+          <img id="poster1" className="PosterImage" src={posterImage1} style={{opacity: poster1opacity}}></img>
+          <img id="poster2" className="PosterImage" src={posterImage2} style={{opacity: poster2opacity}}></img>
         </div>
-
-        
-        
       </header>
     </div>
   );
+
+
+function GetNewPoster(){
+  let newPoster = "https://www.themoviedb.org/t/p/original/456FcvyTujRwzgoMoKKoheeCOlU.jpg";
+  showPoster1 ? setPosterImage2(newPoster) : setPosterImage1(newPoster);
+  }
+  
 }
 
 
