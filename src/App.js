49d6138705c038df4f3ec18894c7c8e5291c6e2e
c13 <chris13ryant@gmail.com>
2021-11-26 13:23:32 -0700

    added poster toggle timer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [posterImage, setPosterImage] = useState("https://www.themoviedb.org/t/p/original/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg");
+  
+  useEffect(() => {
+    const posterTimer = setInterval(() => {
+      TogglePoster();
+    }, 5000);
+    return () => clearInterval(posterTimer);
+  }, []);
+
+  function TogglePoster() {
+    console.log("Toggling Poster");
+    setPosterImage("https://www.themoviedb.org/t/p/original/456FcvyTujRwzgoMoKKoheeCOlU.jpg");
+    
+  };
+  
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Poster Slideshow
-        </p>
+        <div className="Poster-Frame">
+        <img className="PosterImage" src={posterImage}></img>
+
+        </div>
+
         
         
       </header>
     </div>
   );
 }
+
+
 
 export default App;
