ac20bc0a25e956a90e53b7c48da6276b3f748d1b
c13 <chris13ryant@gmail.com>
2021-11-27 11:24:41 -0700

    implemented api calls

--- src/App.js
+++ src/App.js
@@ -7,62 +7,151 @@ function App() {
   const [posterImage2, setPosterImage2] = useState("https://www.themoviedb.org/t/p/original/nx9EtxO7OuOgwmrQdHVgCpBthkw.jpg");
   const [poster1opacity, setPoster1opacity] = useState(1);
   const [poster2opacity, setPoster2opacity] = useState(0);
+  const [messageText, setMessageText] = useState("");
+  const [startYear, setStartYear] = useState(1970);
+  const [endYear, setEndYear] = useState(new Date().getFullYear + 2);
+  const [pageLimit, setPageLimit] = useState(1);
+
 
   let showPoster1 = true;
+  let togglePosters = true;
   let posterToggleTime = 5000;
+  
 
   useEffect(() => {
-    const posterTimer = setInterval(() => {
-      TogglePoster();
-    }, posterToggleTime);
-    return () => clearInterval(posterTimer);
-  }, []);
-
+    function startPosterToggle(){
+      const posterTimer = setInterval(() => {
+        if (togglePosters){
+          togglePoster();
+        }
+        
+      }, posterToggleTime);
+      return () => clearInterval(posterTimer);
+    }
+    startPosterToggle();
+  }, [])
+  
   useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
-  }, []);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+    
+  },[])
+
+  function togglePoster() {
+    
+    setPoster1opacity(+!showPoster1);
+    setPoster2opacity(+showPoster1);
+    showPoster1 = !showPoster1;
+    setTimeout(() => getMovieId(), posterToggleTime/2);
+    //showPoster1 ? console.log("Showing Poster 1") : console.log("Showing Poster 2");
+  }
 
 
   function handleKeyPress(e) {
     console.log(e);
-    if (e.key == " "){
-      console.log('pause');
+    if (e.key === " "){
+      displayMessage("Pause", 2000);
+      togglePosters = !togglePosters;
+    }
+    if (e.key === "g"){
+      getMovieId();
     }
   }
 
-  
-
-  function TogglePoster() {
-    
-    //setPosterImage(GetNewPoster());
-
-    setPoster1opacity(+!showPoster1);
-    setPoster2opacity(+showPoster1);
-
-    showPoster1 = !showPoster1;
-    console.log(showPoster1);
-
-    setTimeout(() => GetNewPoster(), posterToggleTime/2);
+  function displayMessage(message, time){
+    setMessageText(message);
+    setTimeout(() => {setMessageText("")}, time)
   }
+
+  function getMovieId(){
+    console.log("Getting new poster");
+    //let movie_year = getRandInt(startYear, endYear);
+    let movie_year = 2021;
+    let maxPage = 1;
+    let index = getRandInt(0, 19); //each page has 20 results
+    //console.log("getting movie id");
+    let url = "https://api.themoviedb.org/3/discover/movie?api_key="
+    + Data['themoviedb-apikey'] +
+    "&certification_country=US&include_adult=false&certification.ge=PG13&primary_release_year="
+    + movie_year +
+    "&region=US&language=en-US&sort_by=popularity.desc"
+    
+    
+    fetch(url)
+      .then(responce => responce.json())
+      .then(data => {
+        if (pageLimit === 0){
+          maxPage = data.total_pages
+        }
+        else{
+          maxPage = Math.min(pageLimit, data.total_pages)
+        }
+      })
+      .then(() => {
+        let page = getRandInt(1, maxPage);
+        //console.log("Page: " + page)
+        url = url + "&page=" + page;
+
+        fetch(url)
+          .then(responce => responce.json())
+          .then(data => getPosterFromID(data["results"][index]["id"]));
+      
+      });
   
+    
+
+
+
+
+
+
+
+
+
+
+  }
+  function getPosterFromID(movieId){
+    
+    let url = "https://api.themoviedb.org/3/movie/"
+      + movieId + 
+      "/images?api_key="
+      + Data['themoviedb-apikey'];
+
+    
+    let posterImage = "";
+
+    fetch(url)
+      .then(responce => responce.json())
+      .then(data => posterImage = "https://image.tmdb.org/t/p/original/" + data["posters"][getRandInt(0, data["posters"].length)]["file_path"])
+      .then(() => showPoster1 ? setPosterImage2(posterImage) : setPosterImage1(posterImage))
+
+  }
+
+
+
+  function getRandInt(min, max) {
+    return Math.floor(Math.random() * (+max - +min + 1)) + +min;
+  };
+
   
 
   return (
     <div className="App">
       <header className="App-header">
+      <h1 className="message_text">{messageText}</h1>
         <div className="Poster-Frame">
-          <img id="poster1" className="PosterImage" src={posterImage1} style={{opacity: poster1opacity}}></img>
-          <img id="poster2" className="PosterImage" src={posterImage2} style={{opacity: poster2opacity}}></img>
+          <img id="poster1" className="PosterImage" src={posterImage1} style={{opacity: poster1opacity}} alt="poster1"></img>
+          <img id="poster2" className="PosterImage" src={posterImage2} style={{opacity: poster2opacity}} alt="poster2"></img>
         </div>
       </header>
     </div>
   );
 
 
-function GetNewPoster(){
-  let newPoster = "https://www.themoviedb.org/t/p/original/456FcvyTujRwzgoMoKKoheeCOlU.jpg";
-  showPoster1 ? setPosterImage2(newPoster) : setPosterImage1(newPoster);
-  }
+// function GetNewPoster(){
+//   let newPoster = "https://www.themoviedb.org/t/p/original/456FcvyTujRwzgoMoKKoheeCOlU.jpg";
+//   //showPoster1 ? setPosterImage2(newPoster) : setPosterImage1(newPoster);
+//   }
   
 }
 
